1daf86dc954197e093e6d74d9709d598afe8e9f9
Ethan Schroeder <ethan.schroeder@gmail.com>
2008-08-03 05:59:08 +0000

    Sqlite3-related fixes that have grown since 2.3. Closes #2987

--- customerdb/install.php
+++ customerdb/install.php
@@ -14,28 +14,52 @@ if (! function_exists("outn")) {
 global $db;
 global $amp_conf;
 
-$autoincrement = (($amp_conf["AMPDBENGINE"] == "sqlite") || ($amp_conf["AMPDBENGINE"] == "sqlite3")) ? "AUTOINCREMENT":"AUTO_INCREMENT";
-$sql = "
-CREATE TABLE IF NOT EXISTS customerdb 
-(
-	`id` int UNIQUE  $autoincrement, 
-	`name` varchar(45) not null, 
-	`addr1` varchar(150) not null,  
-	`addr2` varchar(150) null, 
-	`city` varchar(50) not null, 
-	`state` varchar(5) not null, 
-	`zip` varchar(12) null, 
-	`sip` varchar(20) null, 
-	`did` varchar(45) null, 
-	`device` varchar(50) null, 
-	`ip` varchar(20) null, 
-	`serial` varchar(50) null, 
-	`account` varchar(6) null, 
-	`email` varchar(150) null, 
-	`username` varchar(25) null, 
-	`password` varchar(25) null
-);
-";
+if($amp_conf["AMPDBENGINE"] == "sqlite3")  {
+	$sql = "
+	CREATE TABLE IF NOT EXISTS customerdb 
+	(
+		`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		`name` varchar(45) not null, 
+		`addr1` varchar(150) not null,  
+		`addr2` varchar(150) null, 
+		`city` varchar(50) not null, 
+		`state` varchar(5) not null, 
+		`zip` varchar(12) null, 
+		`sip` varchar(20) null, 
+		`did` varchar(45) null, 
+		`device` varchar(50) null, 
+		`ip` varchar(20) null, 
+		`serial` varchar(50) null, 
+		`account` varchar(6) null, 
+		`email` varchar(150) null, 
+		`username` varchar(25) null, 
+		`password` varchar(25) null
+	);
+	";
+}
+else  {
+	$sql = "
+	CREATE TABLE IF NOT EXISTS customerdb 
+	(
+		`id` int UNIQUE AUTO_INCREMENT,	 
+		`name` varchar(45) not null, 
+		`addr1` varchar(150) not null,  
+		`addr2` varchar(150) null, 
+		`city` varchar(50) not null, 
+		`state` varchar(5) not null, 
+		`zip` varchar(12) null, 
+		`sip` varchar(20) null, 
+		`did` varchar(45) null, 
+		`device` varchar(50) null, 
+		`ip` varchar(20) null, 
+		`serial` varchar(50) null, 
+		`account` varchar(6) null, 
+		`email` varchar(150) null, 
+		`username` varchar(25) null, 
+		`password` varchar(25) null
+	);
+	";
+}
 $check = $db->query($sql);
 if(DB::IsError($check)) {
 	die_freepbx("Can not create annoucment table");
