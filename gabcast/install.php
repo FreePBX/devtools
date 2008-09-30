8c282f60193320e31f159765c8f2b7973189ad11
Micke Carlsson <mickecamino@gmail.com>
2008-09-30 04:48:57 +0000

    Reverting previous changes, add strings for localization for gabcast

--- gabcast/install.php
+++ gabcast/install.php
@@ -1,9 +1,13 @@
 <?php
+//for translation only
+if (false) {
+_("Connect to Gabcast");
+}
 
 // Enable direct dial to Gabcast as a feature code
 $connecttogabcast = _("Connect to Gabcast");
 $fcc = new featurecode('gabcast', 'gabdial');
-$fcc->setDescription($connecttogabcast);
+$fcc->setDescription('Connect to Gabcast');
 $fcc->setDefault('*422');
 $fcc->update();
 unset($fcc);
