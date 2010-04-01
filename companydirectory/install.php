51ca5ab2cb5fe0c2b0e42a58e1f386a784f0fdcc
Bryan Walters <secretop@gmail.com>
2010-04-01 19:11:27 +0000

    re #4147 - Adds database creation to company directory so we can install this module for testing and use.

--- companydirectory/install.php
+++ companydirectory/install.php
@@ -1,3 +1,56 @@
 <?php
+global $db;
+global $amp_conf;
+
+if (! function_exists("out")) {
+	function out($text) {
+		echo $text."<br />";
+	}
+}
+
+if (! function_exists("outn")) {
+	function outn($text) {
+		echo $text;
+	}
+}
+
+
+$autoincrement = (($amp_conf["AMPDBENGINE"] == "sqlite") || ($amp_conf["AMPDBENGINE"] == "sqlite3")) ? "AUTOINCREMENT":"AUTO_INCREMENT";
+
+outn(_("Adding directory_details table..."));
+
+$sql = "CREATE TABLE directory_details (
+    id INT NOT NULL PRIMARY KEY $autoincrement,
+    dirname varchar(50),
+    description varchar(150),    
+    announcement varchar(50),
+    valid_recording varchar(50),
+    callid_prefix varchar(10),
+    alert_info varchar(50),
+    repeat_loops varchar(3),
+    repeat_recording varchar(50),
+    invalid_recording varchar(50),
+    invalid_destination varchar(50)
+)";
+
+$check = $db->query($sql);
+if (DB::IsError($check)) {
+	die_freepbx("Can not create `directory_details` table" .  $check->getMessage() .  "\n");
+}
+
+outn(_("Adding directory_entries table..."));
+
+$sql = "CREATE TABLE directory_entries (
+    id INT NOT NULL PRIMARY KEY,
+    name varchar(50),
+    type varchar(25),
+    audio varchar(50),
+    dial varchar(50)
+);";
+
+$check = $db->query($sql);
+if (DB::IsError($check)) {
+	die_freepbx("Can not create `directory_entries` table" .  $check->getMessage() .  "\n");
+}
 
 ?>
