45e40636ec8e5ab582be4a58db0188d62c7efa25
Moshe Brevda <mbrevda@gmail.com>
2011-10-04 12:58:22 +0000

    re #5394 - new module package script, aptly called package.php!

---
+++ package.php
@@ -0,0 +1,215 @@
+#!/usr/bin/php -q
+<?php
+
+
+//get cli opts
+$longopts = array(
+	"modules:",
+	"debug::",
+	"checkphp::",
+	'verbose::'
+);
+$vars = getopt('m:d::L::v::');
+
+//move cli args to longopts for clarity throught the script
+if (isset($vars['m'])) {
+	$vars['modules'] = (array) $vars['m'];
+	unset($vars['m']);
+} else {
+	$vars['modules'] = false;
+}
+
+if (isset($vars['d'])) {
+	$vars['debug'] = true;
+	unset($vars['d']);
+} else {
+	$vars['debug'] = false;
+}
+
+if (isset($vars['L'])) {
+	$vars['checkphp'] = false;
+	unset($vars['L']);
+} else {
+	$vars['checkphp'] = true;
+}
+
+if (isset($vars['v'])) {
+	$vars['verbose'] =  true;
+	unset($vars['L']);
+} else {
+	$vars['verbose'] = false;
+}
+
+//find location of md5 command
+if (!$vars['md5'] = run_cmd('which md5sum')) {
+	if (!run_cmd('which md5')) {
+		die("no md5sum command\n");
+	} else {
+		$vars['md5'] = 'md5 -r';
+	}
+}
+
+//set up some other settings
+$vars['rver'] 			= '2.10';
+$vars['fwbranch'] 		= 'branches/2.10';
+$vars['framework']		= 'framework';
+$vars['fw_ari']			= 'fw_ari';
+$vars['fw_langpacks']	= 'fw_langpacks';
+$vars['reldir']			= 'reldir';
+
+//ensure we have modules to package
+if (!$vars['modules']) {
+	die('No modules specified. Please specify them one with the -m option (use multiple switches for more than one module)');
+}
+//print_r($vars);
+//ensure the directory is up to date
+run_cmd('svn up');
+
+foreach ($vars['modules'] as $mod) {
+	$mod 		= trim($mod, '/');
+	$files 		=
+	$filename	=
+	$xml 		=
+	$rawname	=
+	$ver 		= '';
+	
+	echo 'Packaging ' . $mod . '...' . PHP_EOL;
+	$xml = file_get_contents($mod . '/module.xml');
+	
+	//check the xml script integrity
+	include_once('xml2Array.class.php');
+	$parser = new xml2ModuleArray($xml);
+	$xmlarray = $parser->parseAdvanced($xml);
+	//TODO: not sure how to detect a broken xml --MB
+	if (!true) {
+		echo $mod . '/module.xml seems corrupt, ' . $mod . ' won\'t be packaged' . PHP_EOL;
+		continue;
+	}
+	
+	//check that module name is set in module.xml
+	if (!preg_match('/<rawname>(.*?)<\/rawname>/', $xml, $rawname)) {
+		echo $mod . '/module.xml is missing a module name, ' . $mod . ' won\'t be packaged' . PHP_EOL;
+		continue;
+	} else {
+		$rawname = $rawname[1];
+	}
+	
+	//check that module version is set in module.xml
+	if (!preg_match('/<version>(.*?)<\/version>/', $xml, $version)) {
+		echo $mod . '/module.xml is missing a version number, ' . $mod . ' won\'t be packaged' . PHP_EOL;
+		continue;
+	} else {
+		$ver = $version[1];
+	}
+	
+	//check php files for syntaxt errors
+	if ($vars['checkphp']) {
+		$files = scandirr($mod, true);
+		foreach ($files as $f) {
+			if (pathinfo($f, PATHINFO_EXTENSION) == 'php') {
+				if (!system('php -l ' . $f)) {
+					echo('syntaxt error detected in ' . $f . ',' .  $mod . ' won\'t be packaged' . PHP_EOL);
+					continue 2;
+				}
+			}
+		}
+	}
+
+	//check in out standing files
+	if (run_cmd('svn st ' . $mod . '|wc -l') > 1) {
+		run_cmd('svn ci -m "Auto Check-in of any outstanding patches in ' . $mod . '" ' . $mod);
+	}
+	
+	
+	//set tarball name var
+	$filename = $rawname . '-' . $ver . '.tgz';
+	
+	//build tarball
+	run_cmd('tar zcf ' . $filename . ' ' . $mod . ' --exclude ".*" -C ' . $mod );
+	
+	//update md5 sum
+	//TODO!
+	
+	//move tarbal to relase dir
+	run_cmd('mv ' . $filename . ' ../../release/' . $vars['rver']);
+	
+	//add tarball to repository
+	run_cmd('svn add ../../release/' . $vars['rver'] . '/' . $filename);
+	
+	//set mietype of tarball
+	run_cmd('svn ps svn:mime-type application/tgz ../../release/' . $vars['rver'] . '/' . $filename);
+	
+	//set latpublished property
+	$lastpub = run_cmd('svn info ' . $mod . ' | grep Revision: | awk \'{print $2}\'');
+	run_cmd('svn ps lastpublish ' . $lastpub . ' ' . $mod);
+	
+	//check in new tarball and module.xml
+	run_cmd('svn ci ../../release/' . $vars['rver'] . '/' . $filename . ' ' . $mod . '/module.xml'
+					. ' -m"Module package script: ' . $rawname . ' ' . $ver . '"');
+	
+}
+
+/**
+ * function scandirr
+ * scans a directory just like scandir(), only recursively
+ * returns a hierarchical array representing the directory structure
+ *
+ * @pram string - directory to scan
+ * @pram strin - retirn absolute paths
+ * @returns array
+ *
+ * @author Moshe Brevda mbrevda => gmail ~ com
+ */
+function scandirr($dir, $absolute = false) {
+	$list = array();
+	if ($absolute) {
+		global $list;
+	}
+	
+	
+	//get directory contents
+	foreach (scandir($dir) as $d) {
+		
+		//ignore any of the files in the array
+		if (in_array($d, array('.', '..'))) {
+			continue;
+		}
+		
+		//if current file ($d) is a directory, call scandirr
+		if (is_dir($dir . '/' . $d)) {
+			if ($absolute) {
+				scandirr($dir . '/' . $d, $absolute);
+			} else {
+				$list[$d] = scandirr($dir . '/' . $d, $absolute);
+			}
+			
+		
+			//otherwise, add the file to the list
+		} elseif (is_file($dir . '/' . $d) || is_link($dir . '/' . $d)) {
+			if ($absolute) {
+				$list[] = $dir . '/' . $d;
+			} else {
+				$list[] = $d;
+			}
+			
+		}
+	}
+
+	return $list;
+}
+
+function run_cmd($cmd, $quite = false) {
+	global $vars;
+	$quite = $quite ? ' > /dev/null' : '';
+
+	if ($vars['debug']) {
+		echo $cmd . PHP_EOL;
+		return true;
+	} elseif($vars['verbose']) {
+		echo '+ ' . $cmd . PHP_EOL;
+		return system($cmd . $quite);
+	} else {
+		return system($cmd . $quite);
+	}
+}
+?>
