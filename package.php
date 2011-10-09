4108ae4c8b97f7edd5883d1549d64095e6ff5278
Moshe Brevda <mbrevda@gmail.com>
2011-10-09 12:35:59 +0000

    downstream changes

--- package.php
+++ package.php
@@ -34,6 +34,7 @@ $vars['fw_ari']		= 'fw_ari';
 $vars['fw_lang']	= 'fw_langpacks';
 $vars['reldir']		= 'reldir';
 $vars['svn_path']	= 'http://svn.freepbx.org';
+$vars['rm_files']	= array(); //files that will be deleted after the script completes
 
 //move cli args to longopts for clarity throught the script
 //note: once we depend on 5.3, we can refactor this so that either short
@@ -99,54 +100,59 @@ foreach ($vars['modules'] as $mod) {
 	$x			= '';
 	
 	echo 'Packaging ' . $mod . '...' . PHP_EOL;
+	if (!file_exists($mod . '/module.xml')) {
+		echo $mod . '/module.xml dose not exists, ' . $mod . ' will not be built!' . PHP_EOL;
+		continue;
+	}
 	$xml = file_get_contents($mod . '/module.xml');
 	
-	//check the xml script integrity
-	include_once('xml2Array.class.php');
-	$parser = new xml2ModuleArray($xml);
-	$xmlarray = $parser->parseAdvanced($xml);
+	//test xml file and get some of its values
+	list($rawname, $ver) = check_xml($mod, $xml);
+	
+	//dont conitunue if there is an issue with the xml
+	if ($rawname == false || $ver == false) {
+		continue;
+	}
 	
 	//include module specifc hook, if present
 	if (file_exists($mod . '/' . 'package_hook.php')) {
 		echo 'Running ' . $mod . '/' . 'package_hook.php...' . PHP_EOL;
-		include($mod . '/' . 'package_hook.php');
+		
+		//test include so that includes can return false and prevent further execution if it fail
+		if (!include($mod . '/' . 'package_hook.php')) {
+			echo '[FATAL] retrurned from ' . $mod . '/' . 'package_hook.php with an error, ' 
+				. $mod . ' wont be built' . PHP_EOL;
+				continue;
+		}
 	}
 	
 	//include any global hooks, if present
 	if (file_exists('package_hook.php')) {
 		echo 'Running ' . 'package_hook.php...' . PHP_EOL;
-		include('package_hook.php');
+		
+		//test include so that includes can return false and prevent further execution if it fail
+		if (!include('package_hook.php')) {
+			echo '[FATAL] retrurned from  package_hook.php with an error, ' 
+				. $mod . ' wont be built' . PHP_EOL;
+				continue;
+		}
 	}
 	
-	//TODO: not sure how to detect a broken xml --MB
-	//This is fixed now. It will detect broken xml -- tm1000
-	$xml_contents = file_get_contents($mod . '/' . 'module.xml');
-	@$xml_loaded_contents = simplexml_load_string($xml_contents);
-	if($xml_loaded_contents===FALSE) {
-		echo $mod . '/module.xml seems corrupt, ' . $mod . ' won\'t be packaged' . PHP_EOL;
-		continue;
-	}
+	//test xml file and get some of its values. We did this before, but the hooks
+	//may have changed something
+	list($rawname, $ver) = check_xml($mod, $xml);
 	
-	//check that module name is set in module.xml
-	if (!preg_match('/<rawname>(.*?)<\/rawname>/', $xml, $rawname)) {
-		echo $mod . '/module.xml is missing a module name, ' . $mod . ' won\'t be packaged' . PHP_EOL;
+	//dont conitunue if there is an issue with the xml
+	if ($rawname == false || $ver == false) {
 		continue;
-	} else {
-		$rawname = $rawname[1];
-	}
-	
-	//check that module version is set in module.xml
-	if (!preg_match('/<version>(.*?)<\/version>/', $xml, $version)) {
-		echo $mod . '/module.xml is missing a version number, ' . $mod . ' won\'t be packaged' . PHP_EOL;
-		continue;
-	} else {
-		$ver = $version[1];
 	}
 	
 	//check php files for syntax errors
 	if ($vars['checkphp']) {
+		//get a recursivle list of all files
 		$files = scandirr($mod, true);
 		foreach ($files as $f) {
+			//if its a php file, test it
 			if (pathinfo($f, PATHINFO_EXTENSION) == 'php') {
 				if (!system('php -l ' . $f)) {
 					echo('syntaxt error detected in ' . $f . ',' .  $mod . ' won\'t be packaged' . PHP_EOL);
@@ -156,7 +162,7 @@ foreach ($vars['modules'] as $mod) {
 		}
 	}
 	
-	//check in out standing files
+	//check in any out standing files
 	if (run_cmd('svn st ' . $mod . '|wc -l') > 0) {
 		run_cmd('svn ci -m "Auto Check-in of any outstanding changes in ' . $mod . '" ' . $mod);
 	}
@@ -165,44 +171,34 @@ foreach ($vars['modules'] as $mod) {
 	$filename = $rawname . '-' . $ver . '.tgz';
 	
 	//build tarball
-	
 	foreach ($exclude as $ex) {
 		$x .= ' --exclude="' . $ex . '"';
 	}
-	run_cmd('tar zcf ' . $filename . ' ' . $tar_dir . $x . ' -C ' . $tar_dir);
+
+	//if our tar path isnt were we currently are now (i.e. one level up from the module ot be packaged)
+	//tell tar to change directoires (-C) to one level above
+	$tar_dir_path   = explode('/', trim($tar_dir, '/'));
+	$tar_dir        = (is_array($tar_dir_path) && (count($tar_dir_path) > 1))? array_pop($tar_dir_path) : $mod;
+	$tar_dir_path   = (is_array($tar_dir_path) && (count($tar_dir_path) > 1)) 
+					? ' -C /' . implode('/', $tar_dir_path) : '';
+	run_cmd('tar zcf ' . $filename . ' ' . $tar_dir_path . ' ' . $tar_dir . $x );
 	
 	//update md5 sum
-	$module_xml = file_get_contents($mod . '/' . 'module.xml');
-	if(file_exists($filename)) {
-		$md5 = md5_file($filename);
-		$module_xml = preg_replace('/<md5sum>(.*)<\/md5sum>/i','<md5sum>'.$md5.'</md5sum>',$module_xml);
-	} else {
-		echo "No Tarball Package found (in debug mode?)" . PHP_EOL;
-	}
-	/* OLD
 	list($md5) = preg_split('/\s+/', run_cmd($vars['md5'] . ' ' . $filename));
 	run_cmd('sed -i "s|<md5sum>.*</md5sum>|<md5sum>' . $md5 . '</md5sum>|" ' 
 			. $mod . '/module.xml');
-	*/
-
+	
 	//update location
-	/* OLD
 	run_cmd('sed -i "s|<location>.*</location>|<location>release/' . $vars['rver'] . '/' . $filename . '</location>|" ' 
 			. $mod . '/module.xml');
-	*/
-	if(file_exists($filename)) {
-                $module_xml = preg_replace('/<location>(.*)<\/location>/i','<location>' . $vars['rver'] . '/' . $filename . '</location>',$module_xml);
-        }
-
-	file_put_contents($mod . '/' . 'module.xml', $module_xml);
-
+	
 	//move tarbal to relase dir
-	run_cmd('mv ' . $filename . ' ../../release/' . $vars['rver']);
+	run_cmd('mv ' . $filename . ' ../../release/' . $vars['rver'] . '/');
 	
 	//add tarball to release repository
 	run_cmd('svn add ../../release/' . $vars['rver'] . '/' . $filename);
 	
-	//set mietype of tarball
+	//set mimetype of tarball
 	run_cmd('svn ps svn:mime-type application/tgz ../../release/' . $vars['rver'] . '/' . $filename);
 	
 	//set latpublished property
@@ -213,8 +209,13 @@ foreach ($vars['modules'] as $mod) {
 	run_cmd('svn ci ../../release/' . $vars['rver'] . '/' . $filename . ' ' . $mod 
 					. ' -m"Module package script: ' . $rawname . ' ' . $ver . '"');
 					
-	
-	echo $mod . ' version ' . $ver . ' has ben sucsessfuly packaged!' . PHP_EOL;
+	//cleanup any remaining files
+/*	foreach($vars['rm_files'] as $f) {
+		if (file_exists($f)) {
+			run_cmd('rm -rf ' . $f);
+		}
+	}*/
+	echo $mod . ' version ' . $ver . ' has been sucsessfuly packaged!' . PHP_EOL;
 	
 }
 
@@ -267,6 +268,7 @@ function scandirr($dir, $absolute = false) {
 	return $list;
 }
 
+//runs systems commands, or echo's them
 function run_cmd($cmd, $quite = false) {
 	global $vars;
 	$quite = $quite ? ' > /dev/null' : '';
@@ -275,10 +277,41 @@ function run_cmd($cmd, $quite = false) {
 		echo $cmd . PHP_EOL;
 		return true;
 	} elseif($vars['verbose']) {
-		echo '+ ' . $cmd . PHP_EOL;
+		$bt = debug_backtrace();
+		echo PHP_EOL . '+' . $bt[0]["file"] . ':' . $bt[0]["line"] . PHP_EOL;
+		echo "\t" . $cmd . PHP_EOL;
 		return system($cmd . $quite);
 	} else {
 		return system($cmd . $quite);
 	}
+}
+
+//test xml file for validity and extract some info from it
+function check_xml($mod, $xml) {
+	//check the xml script integrity
+	$xml_contents = file_get_contents($mod . '/' . 'module.xml');
+	$xml_loaded_contents = simplexml_load_string($xml_contents);
+	if($xml_loaded_contents === FALSE) { 
+		echo $mod . '/module.xml seems corrupt, ' . $mod . ' won\'t be packaged' . PHP_EOL;
+		return array(false, false);
+	}
+	
+	//check that module name is set in module.xml
+	if (!preg_match('/<rawname>(.*?)<\/rawname>/', $xml, $rawname)) {
+		echo $mod . '/module.xml is missing a module name, ' . $mod . ' won\'t be packaged' . PHP_EOL;
+		$rawname = false;
+	} else {
+		$rawname = $rawname[1];
+	}
+	
+	//check that module version is set in module.xml
+	if (!preg_match('/<version>(.*?)<\/version>/', $xml, $version)) {
+		echo $mod . '/module.xml is missing a version number, ' . $mod . ' won\'t be packaged' . PHP_EOL;
+		$ver = false;
+	} else {
+		$ver = $version[1];
+	}
+	
+	return array($rawname, $ver);
 }
 ?>
