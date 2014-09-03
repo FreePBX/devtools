e94e31608ac09e8da183a5691dcb094859142f32
Andrew Nagy <andrew.nagy@the159.com>
2014-09-03 18:43:32 -0400

    langpacks generator

---
+++ update_fwlangpacks.php
@@ -0,0 +1,81 @@
+#!/usr/bin/php -q
+<?php
+require_once('libraries/freepbx.php');
+$help = array();
+$help[] = array('--directory', 'Directory Location of modules root, always assumed to be ../freepbx from this location');
+$freepbx_conf = freepbx::getFreePBXConfig();
+if (is_array($freepbx_conf) && !empty($freepbx_conf)) {
+	foreach($freepbx_conf as $key => $value) {
+		if (isset($value) && $value != '') {
+			$vars[$key] = $value;
+		}
+	}
+}
+$longopts  = array(
+	"directory::",
+);
+$options = getopt("",$longopts);
+
+$directory = !empty($options['directory']) ? $options['directory'] : $vars['repo_directory'];
+
+if(!file_exists($directory)) {
+  freepbx::out("Directories didn't exist! Aborting");
+  exit(0);
+}
+
+$fwlangpacksReop = $directory . "/fw_langpacks";
+if(!file_exists($fwlangpacksReop) ) {
+	freepbx::out("Language Packs didn't exist! Aborting");
+	exit(0);
+}
+
+foreach(glob($directory."/*", GLOB_ONLYDIR) as $moduleDir) {
+	$module = basename($moduleDir);
+	if($module == 'fw_langpacks') {
+		continue;
+	}
+	freepbx::outn("Checking for i18n folder in ".$module."...");
+	if(!file_exists($moduleDir."/i18n")) {
+		if($module != "framework") {
+			freepbx::out("Not Present, Skipping");
+			continue;
+		} else {
+			freepbx::out("Framework, Special Case");
+		}
+	} else {
+		freepbx::out("found");
+	}
+	try {
+		$repo = Git::open($moduleDir);
+	} catch (Exception $e) {
+		freepbx::out("Unable to open " . $moduleDir . " as a repo");
+		exit(0);
+	}
+
+	$branch = $repo->active_branch();
+	$stashed = $repo->add_stash();
+	$repo->checkout('master');
+	$repo->pull('origin', 'master');
+
+	if($module == "framework") {
+		$moduleDir = $moduleDir . '/amp_conf/htdocs/admin';
+		$module = "amp";
+	}
+	foreach(glob($moduleDir."/i18n/*", GLOB_ONLYDIR) as $langDir) {
+		$lang = basename($langDir);
+		if(file_exists($langDir."/LC_MESSAGES/".$module.".mo")) {
+			$file = $langDir."/LC_MESSAGES/".$module.".mo";
+			if(!file_exists($fwlangpacksReop."/mo/".$lang)) {
+				mkdir($fwlangpacksReop."/mo/".$lang);
+			}
+			freepbx::out("\tProcessing ".$lang." in ".$module);
+			copy($file, $fwlangpacksReop."/mo/".$lang."/".$module.".mo");
+		}
+	}
+
+	$repo->checkout($branch);
+	if($stashed) {
+		$repo->apply_stash();
+		$repo->drop_stash();
+	}
+}
