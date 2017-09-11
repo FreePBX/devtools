2fad53a3397298c6f16aee2e0850862c97256857
Andrew Nagy <andrew.nagy@the159.com>
2017-09-11 19:02:55 -0400

    add updated phpunit

--- phpunit.php
+++ phpunit.php
@@ -9,4 +9,10 @@ if(!file_exists($mod_dir.'/utests')) {
 	echo "No Unit Test Folder!\n";
 	exit(1);
 }
-passthru(__DIR__.'/binaries/phpunit.phar --bootstrap "'.__DIR__.'/phpunitBootstrap.php" '.$config.' '.$mod_dir.'/utests');
+
+if(version_compare(phpversion(), "5.6", ">=")) {
+	$bin = 'phpunit-5.7.21.phar';
+} else {
+	$bin = 'phpunit-4.8.36.phar';
+}
+passthru(__DIR__.'/binaries/'.$bin.' --bootstrap "'.__DIR__.'/phpunitBootstrap.php" '.$config.' '.$mod_dir.'/utests');
