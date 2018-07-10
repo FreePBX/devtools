c822272138ed29733dc23ac9580db7724cb8ca04
Andrew Nagy <andrew.nagy@the159.com>
2018-07-10 14:47:28 -0400

    ability to disable loading freepbx boostrap in phpunit

--- phpunit.php
+++ phpunit.php
@@ -1,10 +1,26 @@
 #!/usr/bin/env php
 <?php
+
+$options = getopt('',array("skipfreepbxbootstrap"));
+
 $config = '';
 $mod_dir = getcwd();
-if(file_exists($mod_dir."/utests/utests.xml")) {
-	$config = "-c ".$mod_dir."/utests/utests.xml";
+if(file_exists($mod_dir."/phpunit.xml")) {
+	$configFile = $mod_dir."/phpunit.xml";
 }
+if(file_exists($mod_dir."/utests/utests.xml")) {
+	$configFile = $mod_dir."/utests/utests.xml";
+}
+if(!empty($configFile)) {
+	$xml = simplexml_load_file($configFile);
+	if(isset($xml['freepbxBootstrap']) && $xml['freepbxBootstrap'] == 'false') {
+		$options['skipbootstrap'] = true;
+	}
+	$config = "-c ".$configFile;
+}
+
+
+
 if(!file_exists($mod_dir.'/utests')) {
 	echo "No Unit Test Folder!\n";
 	exit(1);
@@ -15,4 +31,8 @@ if(version_compare(phpversion(), "5.6", ">=")) {
 } else {
 	$bin = 'phpunit-4.8.36.phar';
 }
-passthru(__DIR__.'/binaries/'.$bin.' --bootstrap "'.__DIR__.'/phpunitBootstrap.php" '.$config.' '.$mod_dir.'/utests');
+if(isset($options['skipbootstrap'])) {
+	passthru(__DIR__.'/binaries/'.$bin.' --bootstrap "'.__DIR__.'/phpunitNoFreePBXBootstrap.php" '.$config.' '.$mod_dir.'/utests');
+} else {
+	passthru(__DIR__.'/binaries/'.$bin.' --bootstrap "'.__DIR__.'/phpunitBootstrap.php" '.$config.' '.$mod_dir.'/utests');
+}
