c6df7dba00d6d20901787c6df34c49c47e4ecbdd
kapil gupta <kgupta@sangoma.com>
2023-11-16 12:08:05 +0530

    updating file

--- github_update_all.sh
+++ github_update_all.sh
@@ -11,18 +11,96 @@
 
 set -ex
 
-declare -a arr=(
+declare -a opensrcmodules=(
 	"superfecta" 
 	"hotelwakeup" 
 	"parking"
+	"dahdiconfig"
+	"announcement"
+	"asterisk-cli"
+	"asteriskinfo"
+	"backup"
+	"blacklist"
+	"callback"
+	"callforward"
+	"callrecording"
+	"callwaiting"
+	"cdr"
+	"cidlookup"
+	"conferences"
+	"core"
+	"customappsreg"
+	"dashboard"
+	"daynight"
+	"dictate"
+	"digiumaddoninstaller"
+	"digium_phones"
+	"directory"
+	"disa"
+	"donotdisturb"
+	"dundicheck"
+	"extensionsettings"
+	"fax"
+	"featurecodeadmin"
+	"findmefollow"
+	"iaxsettings"
+	"infoservices"
+	"ivr"
+	"languages"
+	"logfiles"
+	"manager"
+	"miscapps"
+	"miscdests"
+	"music"
+	"outroutemsg"
+	"paging"
+	"pbdirectory"
+	"phonebook"
+	"pinsets"
+	"printextensions"
+	"queueprio"
+	"queues"
+	"recordings"
+	"restart"
+	"ringgroups"
+	"setcid"
+	"sipsettings"
+	"speeddial"
+	"timeconditions"
+	"tts"
+	"vmblast"
+	"voicemail"
+	"weakpasswords"
+	"framework"
+	"webrtc"
+	"userman"
+	"arimanager"
+	"presencestate"
+	"ucp"
+	"certman"
+	"cxpanel"
+	"contactmanager"
+	"cel"
+	"bulkhandler"
+	"soundlang"
+	"firewall"
+	"configedit"
+	"xmpp"
+	"ttsengines"
+	"calendar"
+	"pm2"
+	"api"
+	"filestore"
+	"amd"
 )
 
-
-# first update framework, because it apparently doesn't update with everything
-# else
-cd /usr/src/freepbx/
-
-for i in "${arr[@]}"; do
+downloadCode() {
+	i=$1
+	echo "$i downloading from git.freepbx.org "
+	./freepbx_git.php -m "$MODULE" -y
+}
+pushToGit() {
+	i=$1
 	cd /usr/src/freepbx/$i
 	echo "pushing $i to Github"
 	git fetch
@@ -32,4 +110,24 @@ for i in "${arr[@]}"; do
 	git push --all github
 	git push --tags github
 	git remote remove github
+}
+
+
+# first update framework, because it apparently doesn't update with everything
+# else
+cd /usr/src/freepbx/
+
+for i in "${opensrcmodules[@]}"; do
+	dir=/usr/src/freepbx/$i
+
+	if [ -d $dir ]; then
+		pushToGit $i 
+	else
+		echo "$i directory does not exists"
+		downloadCode $i 
+		pushToGit $i 
+	fi
 done
+
+
+
